0150bfd38ed644f53193f91d2a28eeaee51b95a4
lvsanli <lvsanli@fangjia.com>
2018-03-29 15:13:11 +0800

    img_comm

--- community_image/download_image.py
+++ community_image/download_image.py
@@ -1,24 +1,45 @@
-import pymongo
+import pika
+import hashlib
+from retry import retry
 import requests
 
-client = pymongo.MongoClient('192.168.0.61', 27017)
-db = client['buildings']
-coll_community = db.get_collection('community_image_url')
-coll_house = db.get_collection('images_community_house')
-count = 0
-for i in coll_community.find({},no_cursor_timeout = True):
-    for img in i['img_list']:
-        response = requests.get(url=img['img'])
-        if response.status_code == 200:
-            count += 1
-            print(count)
+credentials = pika.PlainCredentials('guest1', 'guest1')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+    '192.168.10.190', 5672, '/', credentials))
+
+channel = connection.channel()
+channel.queue_declare(queue='img_url_test')
+
+
+class Download_image:
+    @retry(tries=3)
+    def request_(self, img, md5_url):
+        response = requests.get(img)
+        if response.status_code is 200:
+            with open('image/{0}.png'.format(md5_url), 'wb') as f:
+                f.write(response.content)
+                print('1')
         else:
-            print(response.url)
-for i in coll_house.find():
-    for img in i['img_list']:
-        response = requests.get(url=img['img'])
-        if response.status_code == 200:
-            count += 1
-            print(count)
-        else:
-            print(response.url)
+            print('错误url：', img)
+
+    def callback(self, ch, method, properties, body):
+        img = body.decode()
+        m1 = hashlib.md5()
+        m1.update(img.encode('utf-8'))
+        md5_url = m1.hexdigest()
+        try:
+            self.request_(img, md5_url)
+        except Exception as e:
+            print(e)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def consume_start(self):
+        channel.basic_qos(prefetch_count=1)
+        channel.basic_consume(self.callback,
+                              queue='img_url_test')
+        channel.start_consuming()
+
+
+if __name__ == '__main__':
+    d = Download_image()
+    d.consume_start()
--- community_image/revamp_mongo.py
+++ community_image/revamp_mongo.py
@@ -8,9 +8,10 @@ def get_collection_object(host, port, db_name, collection_name):
     return collection
 
 
-m = get_collection_object('192.168.0.61', 27017, 'buildings', 'house_image_demo')
+m = get_collection_object('192.168.0.61', 27017, 'buildings', 'house_image_demo2')
 for i in m.find():
     _id = i['_id']
+    print(_id)
     image_list = i['image_list']
     img_list = []
     for i in image_list:
@@ -22,4 +23,4 @@ for i in m.find():
             img_list.append(i)
     if not img_list:
         continue
-    m.update({'_id':_id}, {'$set': {"image_list": img_list}}, False, True)
+    m.update({'_id': _id}, {'$set': {"image_list": img_list}}, False, True)
