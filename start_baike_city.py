788782385d57e47bb7d15e10d535d59d7d670596
e <lvsanli@fangjia.com>
2018-04-16 18:25:13 +0800

    baike

--- start_baike_city.py
+++ start_baike_city.py
@@ -2,7 +2,7 @@ from baike_city.baike import baike
 import schedule
 
 if __name__ == '__main__':
-    # schedule.every().sunday.at("12:00").do(baike)
-    # while True:
-    #     schedule.run_pending()
-    baike()
+    schedule.every().sunday.at("12:00").do(baike)
+    while True:
+        schedule.run_pending()
+
