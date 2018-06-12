d5b6057e95390463a351c38a49b1c017eb10f9cc
jijunyu <jijunyu@fangjia.com>
2018-06-12 15:10:50 +0800

    rm qianxi

--- start_baiduqianxi.py
+++
@@ -1,8 +0,0 @@
-from baiduqianxi.qianxi import Baiduqianxi
-import schedule
-
-if __name__ == '__main__':
-    b = Baiduqianxi()
-    schedule.every().day.at("12:00").do(b.start_consume())
-    while True:
-        schedule.run_pending()
