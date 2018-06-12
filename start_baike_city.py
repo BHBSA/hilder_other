587b606ea14bd6ebb7638a14024df667b2a7ef87
jijunyu <jijunyu@fangjia.com>
2018-06-12 15:12:00 +0800

    baike

--- start_baike_city.py
+++
@@ -1,8 +0,0 @@
-from baike_city.baike import crawler_baike
-import schedule
-
-if __name__ == '__main__':
-    schedule.every().sunday.at("12:00").do(crawler_baike)
-    while True:
-        schedule.run_pending()
-
