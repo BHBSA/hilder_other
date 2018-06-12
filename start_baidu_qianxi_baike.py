120812af192fd2806aa530250164443ec7f3b280
jijunyu <jijunyu@fangjia.com>
2018-06-12 15:39:00 +0800

    start baidu qianxi baike

--- start_baidu_qianxi_baike.py
+++ start_baidu_qianxi_baike.py
@@ -4,6 +4,7 @@ import schedule
 
 if __name__ == '__main__':
     b = Baiduqianxi()
+    print('开始百度百科和迁徙')
     schedule.every().day.at("12:00").do(b.start_consume)
     schedule.every().sunday.at("12:00").do(crawler_baike)
     while True:
