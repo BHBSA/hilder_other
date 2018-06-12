7a46d7cdd02f5f536df989c90952298742b14eea
jijunyu <jijunyu@fangjia.com>
2018-06-12 15:13:52 +0800

    start baidu

--- start_baidu_qianxi_baike.py
+++ start_baidu_qianxi_baike.py
@@ -1,8 +1,11 @@
 from baiduqianxi.qianxi import Baiduqianxi
 import schedule
+from baike_city.baike import crawler_baike
 
 if __name__ == '__main__':
     b = Baiduqianxi()
     schedule.every().day.at("12:00").do(b.start_consume)
+    schedule.every().sunday.at("12:00").do(crawler_baike)
     while True:
+        schedule.run_pending()
         schedule.run_pending()
