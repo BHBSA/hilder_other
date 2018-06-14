2827b1068e5d154b8a3681d67d5329c10221465a
jijunyu <jijunyu@fangjia.com>
2018-06-14 13:15:23 +0800

    baike and qianxi

--- start_baidu_qianxi_baike.py
+++ start_baidu_qianxi_baike.py
@@ -5,8 +5,7 @@ import schedule
 if __name__ == '__main__':
     b = Baiduqianxi()
     print('开始百度百科和迁徙')
-    b.start_consume()
-    # schedule.every().day.at("12:00").do(b.start_consume)
-    # schedule.every().sunday.at("12:00").do(crawler_baike)
-    # while True:
-    #     schedule.run_pending()
+    schedule.every().day.at("12:00").do(b.start_consume)
+    schedule.every().sunday.at("12:00").do(crawler_baike)
+    while True:
+        schedule.run_pending()
